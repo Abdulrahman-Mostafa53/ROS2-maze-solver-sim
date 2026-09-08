72060ca640ab5f4d8d299c9731fede9d4f2e7068
ziadelzanaty <ziadelzanaty4@gmail.com>
2026-09-08 21:31:55 +0300

    merged heading and movex PID controllers and added parameters in launch file

--- maze_solver/maze_solver/pid.py
+++ maze_solver/maze_solver/pid.py
@@ -1,53 +1,56 @@
 class Pid:
-    def __init__(self,target, kp, ki, kd,anti_wind_clamp, start=0):
-
-        # intializing parameters passed to the constructor 
-        # pass in your target , KP , KI , KD  anti wind clamp point,
-        # , start point (optional)
+    def __init__(
+        self,
+        target,
+        kp,
+        ki,
+        kd,
+        anti_wind_clamp,
+        controller_clamp,
+        start=0
+    ):
 
         self.TARGET = target
         self.KP = kp
         self.KI = ki
         self.KD = kd
         self.anti_wind_clamp = anti_wind_clamp
+        self.controller_clamp = controller_clamp
         self.START = start
-
-        # in your code after you have a pid object, set :
-
-        # pid.current_state to the current state of the system
-        # ex : a motor rotated 75 degs but we need it to rotate 90 degs so
-        # 75 is current_state 
-
-        # pid.error to the current error val : (target  - current_state)
-
-        # pid.accum_error +=  new error (accumulate your errors here for integral calculation)
 
 
         self.current_state = start
         self.error = target - start
         self.accum_error = 0
-        
-    
-    def compute(self,dt,error_dif):
-        # this function calculates the output signal
-        # if you have done the setup we discussed earlier all you have to provide now is :
-        # error_dif : this is the change in error in some time dt 
-        # error_dif  = e2 - e1
 
-        # dt : the time where error_dif happended
-        # for example if you are running a while loop (but must be restricted
-        # by some time don't live it up to cpu power!) you can define dt as time between two
-        # iterations 
-        
+    def compute(self, dt, error_dif):
 
-        p = round(self.KP * self.error,4)
-        i = min(round(self.KI * self.accum_error,4),self.anti_wind_clamp)
-        d = round(self.KD * (error_dif/dt),4)
 
-        
-        # print("_________________________________________________")
-        # print(
-        #     f"P: {p} -- I: {i} -- D: {d} -- accum : {round(self.accum_error,4)}"
-        # )
-        # print("_________________________________________________")
-        return p+i+d
+        p = round(self.KP * self.error, 4)
+        i = min(
+            round(self.KI * self.accum_error, 4),
+            self.anti_wind_clamp
+        )
+        d = round(
+            self.KD * (error_dif / dt),
+            4
+        )
+        return min(
+            p + i + d,
+            self.controller_clamp
+        )
+
+    def set_kp(self, kp):
+        self.KP = kp
+
+    def set_ki(self, ki):
+        self.KI = ki
+
+    def set_kd(self, kd):
+        self.KD = kd
+
+    def set_anti_wind_clamp(self, clamp):
+        self.anti_wind_clamp = clamp
+
+    def set_controller_clamp(self, clamp):
+        self.controller_clamp = clamp
